b03107160c93b5556658638e2e811c6059286ad4
moru <kevin.singer@student.tuwien.ac.at>
2015-06-24 23:17:28 +0200

    First cctv achievment.

--- app/index.js
+++ app/index.js
@@ -38,19 +38,26 @@ scs.on('change', function(){
         alert(
             "Your advances were so overt and aggravating that the " +
             "populace actually got off their bums to sanction you " +
-            "and your elected buddies via their votes. You probably " +
+            "and your elected buddies with their votes at the urn. You probably " +
             "should have minded their protests. Still, you have " +
             "managed to extract a fine profit of: \n\n" + utils.numberWithCommas(ps.get()) + "$");
         global.window.document.location.reload();
     }
 });
 
+coverAchievmentEarned = false;
+firstLv1AchievmentEarned = false;
 stateStore.on('change', function(){
     console.log("Checking for victory - min: " +
         stateStore.minCctvLevel());
     console.log("Checking for victory - max: " +
         stateStore.maxCctvLevel());
-    if (stateStore.minCctvLevel() >= 1) { //TODO should be higher
+    if (stateStore.maxCctvLevel() == 1 && !firstLv1AchievmentEarned) {
+        firstLv1AchievmentEarned = true;
+        alert("Your first surveillance-sytem! The first-step of your corporate empire.");
+    }
+    if (stateStore.minCctvLevel() >= 1 && !coverAchievmentEarned) { //TODO should be higher
+        coverAchievmentEarned = true;
         alert("You've covered the US with your surveillance systems! (" +
             "And made fine profit while doing so: " +
             utils.numberWithCommas(ps.get()) + "$)");
